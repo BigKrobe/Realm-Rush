05d9ee1d21ffdf93d188ad7de0808200417d9e85
DERP-DESKTOP\Nate <natebrks@gmail.com>
2020-12-06 18:04:18 +1000

    Added text to show world space pos

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float _gridSize = 10f;
-    // Update is called once per frame
+
+    TextMesh _textMesh;
+
+    private void Start()
+    {
+
+    }
+
     void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / 10f) * _gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / 10f) * _gridSize;
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        _textMesh = GetComponentInChildren<TextMesh>();
+        _textMesh.text = snapPos.x / _gridSize + ", " + snapPos.z / _gridSize;
     }
 }

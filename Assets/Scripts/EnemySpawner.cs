e2f15449d7688bcc59355fa85a2191d98ad6bfe7
DERP-DESKTOP\Nate <natebrks@gmail.com>
2020-12-17 10:28:08 +1000

    Display Health and Score

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -8,11 +9,15 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] int _enemiesToSpawn = 10;
     [SerializeField] EnemyMovement _enemyPrefab;
     [SerializeField] GameObject _enemyParent;
+    [SerializeField] int _numEnemySpawned;
+    [SerializeField] Text _enemySpawned;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnEnemy());
+        _numEnemySpawned = _enemiesToSpawn;
+        _enemySpawned.text = _numEnemySpawned.ToString();
     }
 
     // Update is called once per frame
@@ -23,11 +28,19 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemy()
     {
-        for (int i = _enemiesToSpawn; i > 0; i--) 
+        for (int i = _enemiesToSpawn; i > 0; i--)
         {
+            AddScore();
             var newEnemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
             newEnemy.transform.parent = _enemyParent.transform;
             yield return new WaitForSeconds(_secondsBetweenSpawns);
+
         }
+    }
+
+    private void AddScore()
+    {
+        _numEnemySpawned--;
+        _enemySpawned.text = _numEnemySpawned.ToString();
     }
 }
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int _playerHealth = 10;
+    [SerializeField] int _healthDecrease = 1;
+    [SerializeField] Text _healthText;
+
+    private void Start()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _playerHealth -= _healthDecrease;
+        _healthText.text = _playerHealth.ToString();
+
+    }
+
+
+
+}
